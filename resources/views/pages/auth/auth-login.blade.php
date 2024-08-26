bf01dc71cc96f879566f70feb6deb13b509e8574
FarizSatria <farizsatria18@gmail.com>
2024-08-26 22:47:33 +0700

    tambah tombol

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -49,6 +49,11 @@
                     tabindex="4">
                     Login
                 </button>
+                <button type="submit"
+                    class="btn btn-primary btn-lg btn-block"
+                    tabindex="4">
+                    Keluar
+                </button>
             </div>
         </form>
     </div>
